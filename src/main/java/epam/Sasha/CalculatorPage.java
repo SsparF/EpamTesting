fc6306e5ca2de05e16339a208c50aa14605b5640
Sasha <sashaparfiak007@gmail.com>
2021-11-28 00:27:17 +0300

    Finally version with Ðuniversal methods

--- src/main/java/epam/Sasha/CalculatorPage.java
+++ src/main/java/epam/Sasha/CalculatorPage.java
@@ -9,14 +9,16 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.security.Key;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class CalculatorPage extends BasePage{
 
     private By title=new By.ByXPath("//h2[contains (text(),'Google Cloud Pricing Calculator')]");
     private String iframe="myFrame";
-    private By section=new By.ByXPath("(//div[@title='Compute Engine'])[1]");
+    private By sectionСhoose = new By.ByXPath("//md-tabs-canvas//md-pagination-wrapper//md-tab-item//div[@class='name ng-binding']");
     //first block
     private FirstBlockPage firstBlockPage=new FirstBlockPage();
     //second block
@@ -40,58 +42,157 @@ public class CalculatorPage extends BasePage{
         return driver.findElement(title).isDisplayed();
     }
 
-    public void selectSection() {
-        driver.findElement(section);
+    public void selectSection(String sec) {
+        List<WebElement> sections = driver.findElements(sectionСhoose);
+        List<String> sectionName = sections.stream().map(el -> el.getText()).collect(Collectors.toList());
+        for (int i=0; i<sections.size(); i++) {
+            if (sectionName.get(i).contains(sec)) {
+                sections.get(i).click();
+                break;
+            }
+        }
     }
 
-    public void fillNumOfInstances() {
+    public void fillNumOfInstances(String num) {
         WebElement instances=driver.findElement(firstBlockPage.numberInstances);
         instances.click();
-        instances.sendKeys("4");
+        instances.sendKeys(num);
     }
 
-    public void fillMachineType() {
+    public void chooseSoftware(String sft) throws InterruptedException {
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(firstBlockPage.software));
+        driver.findElement(firstBlockPage.software).click();
+        Thread.sleep(3000);
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(firstBlockPage.chooseSoftware));
+        List<WebElement> allSoftware = driver.findElements(firstBlockPage.chooseSoftware);
+        List<String> sectionName = allSoftware.stream().map(el -> el.getText()).collect(Collectors.toList());
+        for (int i=0; i<allSoftware.size(); i++) {
+            if (sectionName.get(i).contains(sft)) {
+                allSoftware.get(i).click();
+                break;
+            }
+        }
+    }
+
+    public void fillMachineClass(String oneClass) throws InterruptedException {
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(firstBlockPage.machineClass));
+        driver.findElement(firstBlockPage.machineClass).click();
+        Thread.sleep(3000);
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(firstBlockPage.chooseMachineClass));
+        List<WebElement> allClasses = driver.findElements(firstBlockPage.chooseMachineClass);
+        List<String> className = allClasses.stream().map(el -> el.getText()).collect(Collectors.toList());
+        for (int i=0; i<allClasses.size(); i++) {
+            if (className.get(i).contains(oneClass)) {
+                allClasses.get(i).click();
+                break;
+            }
+        }
+    }
+
+    public void fillMachineType(String type) {
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.elementToBeClickable(firstBlockPage.machineType));
         driver.findElement(firstBlockPage.machineType).click();
-        driver.findElement(firstBlockPage.selectedMachineType).click();
+        List<WebElement> allTypes = driver.findElements(firstBlockPage.selectedMachineType);
+        List<String> sectionName = allTypes.stream().map(el -> el.getText()).collect(Collectors.toList());
+        for (int i=0; i<allTypes.size(); i++) {
+            if (sectionName.get(i).contains(type)) {
+                allTypes.get(i).click();
+                break;
+            }
+        }
     }
 
     public void clickEddToEstimate1() {
         driver.findElement(firstBlockPage.addEstimate1).click();
     }
 
-    public void fillNumOfNodes() {
+    public void fillNumOfNodes(String num) {
         WebElement nodes=driver.findElement(secondBlockPage.numberNodes);
         nodes.click();
-        nodes.sendKeys("2");
+        nodes.sendKeys(num);
     }
 
     public void clickAddGpu() {
         driver.findElement(secondBlockPage.addGPU).click();
     }
 
-    public void fillGpuType() {
+    public void fillGpuType(String type) throws InterruptedException {
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(secondBlockPage.gPUType));
         driver.findElement(secondBlockPage.gPUType).click();
-        driver.findElement(secondBlockPage.selectGPUType).click();
+        Thread.sleep(3000);
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(secondBlockPage.selectGPUType));
+        List<WebElement> allTypes = driver.findElements(secondBlockPage.selectGPUType);
+        List<String> typesName = allTypes.stream().map(el -> el.getText()).collect(Collectors.toList());
+        for (int i=0; i<allTypes.size(); i++) {
+            if (typesName.get(i).contains(type)) {
+                allTypes.get(i).click();
+                break;
+            }
+        }
     }
 
-    public void fillNumOfGpu() {
+    public void fillNumOfGpu(String num) throws InterruptedException {
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(secondBlockPage.numberGPU));
         driver.findElement(secondBlockPage.numberGPU).click();
-        driver.findElement(secondBlockPage.selectNumberGPU).click();
+        Thread.sleep(3000);
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(secondBlockPage.selectNumberGPU));
+        List<WebElement> allNums = driver.findElements(secondBlockPage.selectNumberGPU);
+        List<String> numsName = allNums.stream().map(el -> el.getText()).collect(Collectors.toList());
+        for (int i=0; i<allNums.size(); i++) {
+            if (numsName.get(i).contains(num)) {
+                allNums.get(i).click();
+                break;
+            }
+        }
     }
 
     public void clickCpuOverCommit() {
         driver.findElement(secondBlockPage.cPUOverCommit).click();
     }
 
-    public void fillDataCenterLocation() {
-        driver.findElement(secondBlockPage.dataCenterLocation).click();
-        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.elementToBeClickable(secondBlockPage.selectDataCenterLocation));
-        driver.findElement(secondBlockPage.selectDataCenterLocation).click();
+    public void fillLocalSSD(String ssd) throws InterruptedException {
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(secondBlockPage.localSSD));
+        driver.findElement(secondBlockPage.localSSD).click();
+        Thread.sleep(3000);
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(secondBlockPage.selectLocalSSD));
+        List<WebElement> allSSD = driver.findElements(secondBlockPage.selectLocalSSD);
+        List<String> sSDName = allSSD.stream().map(el -> el.getText()).collect(Collectors.toList());
+        for (int i=0; i<allSSD.size(); i++) {
+            if (sSDName.get(i).contains(ssd)) {
+                allSSD.get(i).click();
+                break;
+            }
+        }
     }
 
-    public void fillCommittedUsage() {
+    public void fillDataCenterLocation(String dtc) throws InterruptedException {
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(secondBlockPage.dataCenterLocation));
+        driver.findElement(secondBlockPage.dataCenterLocation).click();
+        Thread.sleep(3000);
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(secondBlockPage.selectDataCenterLocation));
+        List<WebElement> allDataCentres = driver.findElements(secondBlockPage.selectDataCenterLocation);
+        List<String> dataCentresName = allDataCentres.stream().map(el -> el.getText()).collect(Collectors.toList());
+        for (int i=0; i<allDataCentres.size(); i++) {
+            if (dataCentresName.get(i).contains(dtc)) {
+                allDataCentres.get(i).click();
+                break;
+            }
+        }
+    }
+
+    public void fillCommittedUsage(String usg) throws InterruptedException {
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(secondBlockPage.committedUsage));
         driver.findElement(secondBlockPage.committedUsage).click();
-        driver.findElement(secondBlockPage.selectCommittedUsage).click();
+        Thread.sleep(3000);
+        WaitUntil(TimeConstants.PAGE_LOAD_TIMEOUT, ExpectedConditions.presenceOfElementLocated(secondBlockPage.selectCommittedUsage));
+        List<WebElement> allUsages = driver.findElements(secondBlockPage.selectCommittedUsage);
+        List<String> usagesName = allUsages.stream().map(el -> el.getText()).collect(Collectors.toList());
+        for (int i=0; i<allUsages.size(); i++) {
+            if (usagesName.get(i).contains(usg)) {
+                allUsages.get(i).click();
+                break;
+            }
+        }
     }
 
     public void clickEddToEstimate2() {
--- src/main/java/epam/Sasha/FirstBlockPage.java
+++ src/main/java/epam/Sasha/FirstBlockPage.java
@@ -4,7 +4,11 @@ import org.openqa.selenium.By;
 
 public class FirstBlockPage {
     public By numberInstances=new By.ByXPath("//input[@id='input_75']");
+    public By software=new By.ByXPath("//md-select-value[@id='select_value_label_67']");
+    public By chooseSoftware=new By.ByXPath("//div[@id='select_container_89']//div");
+    public By machineClass=new By.ByXPath("//md-select-value[@id='select_value_label_68']");
+    public By chooseMachineClass=new By.ByXPath("//div[@id='select_container_93']//div");
     public By machineType=new By.ByXPath("//md-select-value[@id='select_value_label_71']");
-    public By selectedMachineType=new By.ByXPath("//md-option[@id='select_option_272']");
+    public By selectedMachineType=new By.ByXPath("//md-select-menu[@class='md-overflow']//div[@class='md-text' or @class='md-text ng-binding']");
     public By addEstimate1=new By.ByXPath("(//button[@class='md-raised md-primary cpc-button md-button md-ink-ripple'])[1]");
 }
